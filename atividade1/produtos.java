6ca98d249fcd89050e05bb7fc9dd7cdda529b709
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-17 02:23:58 -0300

    Add produtos class with methods for presentation and sales

--- atividade1/produtos.java
+++ atividade1/produtos.java
@@ -1,1 +1,20 @@
 
+package atividade1;
+
+public class produtos {
+    Double valor;
+    String nome;
+    int estoque;
+
+    void apresentar(){
+        System.out.println("Produto: "+ nome + " Preço: "+ valor + " Estoque: " + estoque);
+    }
+    void vender(int qtd) {
+        if (qtd > estoque){
+            System.out.println("- Infelizmente o nosso estoque acabou");
+        }
+        else{
+            estoque = estoque - qtd;
+        }
+    }
+}
