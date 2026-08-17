6b3cc375183d512cb880cde6d83f0b67bbf71a5e
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-17 02:23:31 -0300

    Add main method to manage product sales

--- atividade1/principal.java
+++ atividade1/principal.java
@@ -1,1 +1,35 @@
 
+package atividade1;
+
+public class principal {
+    public static void main(String[] strg){
+        produtos p1 = new produtos();
+        p1.valor = 10.00;
+        p1.nome = "2KG Arroz";
+        p1.estoque = 1;
+
+        produtos p2 = new produtos();
+        p2.valor = 9.00;
+        p2.nome = "1L Azeite";
+        p2.estoque = 12;
+
+        produtos p3 = new produtos();
+        p3.valor = 7.00;
+        p3.nome = "1L Leite";
+        p3.estoque = 24;
+
+        System.out.println("=-=-=-=- Antes da venda =-=-=-=-=-");
+        p1.apresentar();
+        p2.apresentar();
+        p3.apresentar();
+
+
+        System.out.println("=-=-=-=-- Depois da venda =--=--=-=-");
+        p1.vender(2);
+
+        p1.apresentar();
+        p2.apresentar();
+        p3.apresentar();
+
+    }
+}
