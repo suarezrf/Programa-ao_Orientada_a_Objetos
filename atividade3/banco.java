bfe2e1aa1611b4806a3e99318f975d259d2ad0ff
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-17 02:24:59 -0300

    Add banco class with deposit and withdraw methods

--- atividade3/banco.java
+++ atividade3/banco.java
@@ -1,1 +1,26 @@
 
+package atividade3;
+
+public class banco {
+    String Titular;
+    double saldo;
+
+    void Exibir(){
+        System.out.println("=-=-=-=-=- Banco =-=-=-=-=-=");
+        System.out.println("- Titular: "+Titular );
+        System.out.println("- Saldo: "+saldo );
+        System.out.println("----------------------------");
+
+    }
+    void depositar(double valor){
+        saldo += valor;
+    }
+    void sacar(double valor) {
+        if (saldo < valor) {
+            System.out.println("Saque invalido!");
+    } else {
+        saldo -= valor;
+        }
+    }
+
+}
