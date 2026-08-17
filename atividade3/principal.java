8ca441706901bbafcba386abbabfb8ce64e668eb
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-17 02:25:13 -0300

    Implement main method for banco class operations

--- atividade3/principal.java
+++ atividade3/principal.java
@@ -1,1 +1,24 @@
 
+package atividade3;
+
+public class principal {
+    public static void main(String[] strg) {
+        banco b1 = new banco();
+        b1.Titular = "Carlos Titular";
+        b1.saldo = 100;
+
+        banco b2 = new banco();
+        b2.Titular = "Daniel Crimes Ciberneticos";
+        b2.saldo = 1000;
+
+        b1.Exibir();
+        b2.Exibir();
+
+        b1.sacar(10);
+        b2.depositar(100);
+
+        b1.Exibir();
+        b2.Exibir();
+
+    }
+}
