dae315098642c20345b13cb083c1d6850a45ba62
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-17 02:24:25 -0300

    Add filme class with properties and methods

--- atividade2/filme.java
+++ atividade2/filme.java
@@ -1,1 +1,30 @@
 
+package atividade2;
+
+public class filme {
+    String Titulo;
+    int duracao;
+    double nota;
+    boolean assistido;
+
+    void Exibir(){
+        System.out.println("=--=-=-=-=-= Filme =-=-=-=-=-=" );
+        System.out.println("Titulo: " +Titulo );
+        System.out.println("Duraçao do filme: " +duracao+ "h" );
+        System.out.println("Nota: " + nota );
+        System.out.println("Assistido: " + assistido );
+    }
+    void marcarassistido(){
+        assistido = true;
+    }
+
+    void trocarnota(double novaNota){
+        if (nota >= 0 && nota <=10 ) {
+            nota = novaNota;
+            System.out.println("Nota Válida!");
+
+        }else {
+            System.out.println("Nota Inválida!");
+        }
+    }
+}
