5fdb188cd58c71a54938c1ea56577368b70b85c2
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-18 15:07:03 -0300

    Add produto class with name and price attributes

---
+++ aula2/atividade1/produto.java
@@ -0,0 +1,11 @@
+package Atividade4;
+
+public class produto {
+    String nome;
+    double preco;
+
+    produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
