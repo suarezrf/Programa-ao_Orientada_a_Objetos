c30aac62b6de67c41db4bb00ff1e04f4bfc84dc6
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-18 15:11:23 -0300

    Add Autor class with name and nationality fields

---
+++ aula2/atividade3/Autor.java
@@ -0,0 +1,12 @@
+package Atividade6;
+
+public class Autor {
+    String nome;
+    String nacionalidade;
+
+    Autor(String nome, String nacionalidade){
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+
+    }
+}
