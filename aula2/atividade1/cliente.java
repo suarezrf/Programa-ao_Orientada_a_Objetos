fe58898dc2575e7482593aa8c1c702aab86fe8ef
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-18 15:08:03 -0300

    Add cliente class with name and email fields

---
+++ aula2/atividade1/cliente.java
@@ -0,0 +1,13 @@
+package Atividade4;
+
+public class cliente {
+    String nome;
+    String email;
+
+    cliente(String nome, String email){
+        this.nome = nome;
+        this.email = email;
+
+    }
+
+}
