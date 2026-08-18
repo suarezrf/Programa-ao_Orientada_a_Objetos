1d379321d641936b4ee7b914c56fb0463372ae8e
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-18 15:10:05 -0300

    Create Paciente class with name and phone attributes

---
+++ aula2/atividade2/Paciente.java
@@ -0,0 +1,12 @@
+package Atividade5;
+
+public class Paciente {
+    String nome;
+    String telefone;
+
+    Paciente(String nome, String telefone){
+        this.nome = nome;
+        this.telefone = telefone;
+
+    }
+}
