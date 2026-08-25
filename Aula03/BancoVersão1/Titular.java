a043517be2f90ac93fe451416ba91e5b9bb815df
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-25 16:33:06 -0300

    Add Titular class for bank account holder

---
+++ Aula03/BancoVersão1/Titular.java
@@ -0,0 +1,13 @@
+package atividade3;
+
+public class Titular {
+    String nome;
+    String cpf;
+    String dataNasc;
+
+    Titular(String nome, String cpf, String dataNasc) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNasc = dataNasc;
+    }
+}
