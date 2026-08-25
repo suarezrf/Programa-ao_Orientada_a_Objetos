bcbd7bfb0cf3f143d0c7600cfe31ede97d25ab52
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-08-25 16:47:55 -0300

    Add Principal class to launch Caixa menu

---
+++ Aula03/BancoVersão3/Principal.java
@@ -0,0 +1,8 @@
+package Atividade7;
+
+public class Principal {
+    public static void main(String[] args) {
+        // Redireciona o início da aplicação para o menu do Caixa
+        Caixa.main(args);
+    }
+}
